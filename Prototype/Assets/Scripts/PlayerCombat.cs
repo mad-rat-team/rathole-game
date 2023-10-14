ffe15c2d20909cc772f5f271aa2434afc7722b78
Zhizhoid <misha2006.mk@gmail.com>
2023-10-14 15:50:32 +0300

    Update player's attack

--- Prototype/Assets/Scripts/PlayerCombat.cs
+++ Prototype/Assets/Scripts/PlayerCombat.cs
@@ -1,33 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    [SerializeField] GameObject attackObject;
+    [SerializeField] GameObject attackTrail;
+    [SerializeField] SpriteRenderer attackTrailSpriteRenderer;
     [SerializeField] LayerMask enemiesLayerMask;
-    [SerializeField] Collider2D attackColl;
+
+    private Collider2D attackColl;
+    private Animator attackTrailAnimator;
+
+    private void Awake()
+    {
+        if (!attackTrail.TryGetComponent<Collider2D>(out attackColl))
+        {
+            Debug.LogError("attackTrail does not have a Collider2D component");
+        }
+
+        if (!attackTrail.TryGetComponent<Animator>(out attackTrailAnimator))
+        {
+            Debug.LogError("attackTrail does not have an Animator component");
+        }
+    }
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             // Casting camera world pos to Vector2 is necessary so that its Z component doesn't affect the calculation
-            Vector2 dir = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)attackObject.transform.position).normalized;
+            Vector2 dir = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)attackTrail.transform.position).normalized;
             Attack(dir);
         }
 
-        Debug.DrawRay(
-            attackObject.transform.position,
-            ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)(attackObject.transform.position)).normalized,
-            Color.white
-            );
+        //Debug.DrawRay(
+        //    attackTrail.transform.position,
+        //    ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)(attackTrail.transform.position)).normalized,
+        //    Color.white
+        //    );
     }
 
     private void Attack(Vector2 dir)
     {
-        Debug.Log($"Attacked to {dir}");
-        attackObject.transform.eulerAngles = Vector3.forward * Vector2.SignedAngle(Vector2.right, dir);
-        attackObject.GetComponentInChildren<Animator>().SetTrigger("Attacked"); // Delete this ASAP; Make attackObject handle its animation;
+        //Debug.Log($"Attacked to {dir}");
+        float attackAngle = Vector2.SignedAngle(Vector2.right, dir);
+        attackTrail.transform.eulerAngles = Vector3.forward * attackAngle;
+        attackTrailSpriteRenderer.flipY = Mathf.Abs(attackAngle) > 90;
+
+        attackTrailAnimator.SetTrigger("Attacked");
     }
 }
