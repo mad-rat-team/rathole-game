f06ab273e1b8cf8c3cd5fa80fbb493f816d2c94a
Zhizhoid <misha2006.mk@gmail.com>
2023-10-17 17:05:00 +0300

    Change attackTrail hierarchy; Change player's proportions

--- Prototype/Assets/Scripts/PlayerCombat.cs
+++ Prototype/Assets/Scripts/PlayerCombat.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    [SerializeField] GameObject attackTrailRotator;
     [SerializeField] GameObject attackTrail;
-    [SerializeField] SpriteRenderer attackTrailSpriteRenderer;
     [SerializeField] Camera playerCamera;
     [SerializeField] ContactFilter2D enemiesContactFilter;
 
-    private Collider2D attackColl;
+    private Collider2D attackTrailColl;
+    private SpriteRenderer attackTrailSpriteRenderer;
     private Animator attackTrailAnimator;
 
     private void Awake()
     {
-        if (!attackTrail.TryGetComponent<Collider2D>(out attackColl))
+        if (!attackTrail.TryGetComponent<Collider2D>(out attackTrailColl))
         {
             Debug.LogError("attackTrail does not have a Collider2D component");
         }
 
+        if (!attackTrail.TryGetComponent<SpriteRenderer>(out attackTrailSpriteRenderer))
+        {
+            Debug.LogError("attackTrail does not have a SpriteRenderer component");
+        }
+        
         if (!attackTrail.TryGetComponent<Animator>(out attackTrailAnimator))
         {
             Debug.LogError("attackTrail does not have an Animator component");
@@ -30,13 +36,13 @@ public class PlayerCombat : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             // Casting camera world pos to Vector2 is necessary so that its Z component doesn't affect the calculation
-            Vector2 dir = ((Vector2)playerCamera.ScreenToWorldPoint(Input.mousePosition) - (Vector2)attackTrail.transform.position).normalized;
+            Vector2 dir = ((Vector2)playerCamera.ScreenToWorldPoint(Input.mousePosition) - (Vector2)attackTrailRotator.transform.position).normalized;
             Attack(dir);
         }
 
         //Debug.DrawRay(
         //    attackTrail.transform.position,
-        //    ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)(attackTrail.transform.position)).normalized,
+        //    ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)(attackTrailRotator.transform.position)).normalized,
         //    Color.white
         //    );
     }
@@ -45,16 +51,16 @@ public class PlayerCombat : MonoBehaviour
     {
         //Debug.Log($"Attacked to {dir}");
         float attackAngle = Vector2.SignedAngle(Vector2.right, dir);
-        attackTrail.transform.eulerAngles = Vector3.forward * attackAngle;
+        attackTrailRotator.transform.eulerAngles = Vector3.forward * attackAngle;
 
-        Physics2D.SyncTransforms(); // Force update collider's position according to attackTrail's rotation
+        Physics2D.SyncTransforms(); // Force update collider's position according to attackTrailRotator's rotation
         List<Collider2D> enemyColliders = new List<Collider2D>();
-        int enemyColliderCount = attackColl.OverlapCollider(enemiesContactFilter, enemyColliders);
+        attackTrailColl.OverlapCollider(enemiesContactFilter, enemyColliders);
 
         foreach (var enemyColl in enemyColliders)
         {
             Enemy enemy = enemyColl.GetComponent<Enemy>();
-            enemy.TakeHit(attackTrail.transform.position, 1000f);
+            enemy.TakeHit(attackTrailRotator.transform.position, 1000f);
         }
 
         attackTrailSpriteRenderer.flipY = Mathf.Abs(attackAngle) > 90;
