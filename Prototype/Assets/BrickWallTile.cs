b5d6a107d8a00cf5e81dd3a702c13c56c5851d30
Zhizhoid <misha2006.mk@gmail.com>
2023-12-20 20:14:17 +0200

    Continue working on dynamic wall tile

--- Prototype/Assets/BrickWallTile.cs
+++ Prototype/Assets/BrickWallTile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -10,10 +11,70 @@ public class BrickWallTile : TileBase
     [SerializeField] private Sprite[] brickLeftBackSprites;
     [SerializeField] private Sprite[] brickRightFrontSprites;
     [SerializeField] private Sprite[] brickRightBackSprites;
+    [SerializeField] private Sprite[] brickHalfSprites;
     [SerializeField] private int seed = 0;
+
+    private enum BrickType
+    {
+        Null,
+        //Unknown,
+        LeftFront,
+        LeftBack,
+        RightFront,
+        RightBack,
+        HalfBrick
+    }
+
+    private BrickType[,,] bricks;
+
+    private class TilePosComparer : IComparer<Vector3Int>
+    {
+        public int Compare(Vector3Int x, Vector3Int y)
+        {
+            return 0;
+        }
+    }
+
 
     public override void RefreshTile(Vector3Int position, ITilemap tilemap)
     {
+        //Vector3 worldPos = tilemap.GetComponent<Tilemap>().CellToWorld(position);
+        ////Debug.Log(worldPos);
+        //Debug.DrawLine(worldPos, worldPos + Vector3.up * 0.5f, Color.red, 1);
+
+        //Debug.Log($"{tilemap.size} {tilemap.cellBounds.size}");
+
+        //foreach(Vector3Int bi in tilemap.cellBounds.allPositionsWithin)
+        //{
+        //    Vector3 worldPos = tilemap.GetComponent<Tilemap>().CellToWorld(bi);
+        //    Debug.DrawLine(worldPos, worldPos + Vector3.up * 0.5f, Color.red, 1);
+        //}
+
+        bricks = new BrickType[tilemap.size.x, tilemap.size.y, tilemap.size.z];
+
+
+        SortedSet<Vector3Int> unknownBricks = new();
+
+        foreach (Vector3Int tilePos in tilemap.cellBounds.allPositionsWithin)
+        {
+            if (tilemap.GetTile(tilePos) == null) continue;
+            //bricks[tilePos.x, tilePos.y, tilePos.z] = BrickType.Unknown;
+        }
+
+        Vector3 worldPos = tilemap.GetComponent<Tilemap>().CellToWorld(new Vector3Int(tilemap.cellBounds.xMax - 1, tilemap.cellBounds.yMax - 1, 0));
+        Debug.DrawLine(worldPos, worldPos + Vector3.up * 0.5f, Color.red, 1);
+
+        //for (int x = 0; x < tilemap.size.x; x++)
+        //{
+        //    for (int y = 0; y < tilemap.size.y; y++)
+        //    {
+        //        Vector3 worldPos = tilemap.GetComponent<Tilemap>().CellToWorld(new Vector3Int(x + tilemap.origin.x, y + tilemap.origin.y, 0));
+        //        //Debug.Log(worldPos);
+        //        Debug.DrawLine(worldPos, worldPos + Vector3.up * 0.5f, Color.red, 1);
+        //    }
+        //}
+
+
         for (int dx = -1; dx <= 1; dx++)
         {
             for (int dy = -1; dy <= 1; dy++)
@@ -27,6 +88,9 @@ public class BrickWallTile : TileBase
     {
         base.GetTileData(position, tilemap, ref tileData);
         tileData.sprite = GetRandomSprite(brickLeftFrontSprites, position);
+
+        //Vector3 worldPos = tilemap.GetComponent<Tilemap>().CellToWorld(position);
+        //Debug.DrawLine(worldPos, worldPos + Vector3.up * 0.25f, Color.blue, 1);
 
         //bool isLeftBrick = true;
     }
