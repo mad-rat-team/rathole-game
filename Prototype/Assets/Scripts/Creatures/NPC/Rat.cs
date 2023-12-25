8c20014f77575eabdf9a28308fdc490a527a2b1c
Zhizhoid <misha2006.mk@gmail.com>
2023-12-25 12:48:48 +0200

    Delete outdated experimental assets; Change rat's jumpTime

--- Prototype/Assets/Scripts/Creatures/NPC/Rat.cs
+++ Prototype/Assets/Scripts/Creatures/NPC/Rat.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Rat : MonoBehaviour
 {
     [SerializeField] private float walkSpeed = 1f;
+    [SerializeField] private float jumpTime = 1f;
 
     private Movement movement;
     private Health health;
@@ -28,17 +29,11 @@ public class Rat : MonoBehaviour
         player = GameObject.FindWithTag("Player"); //PH
     }
 
-    //public bool isMoving = false; //TEST
-    //private void Start()
-    //{
-    //    movement.SetIsMoving(isMoving);
-    //}
-
     private void Update()
     {
         if (Shortcuts.IsoToReal(player.transform.position - transform.position).sqrMagnitude < 3 * 3)
         {
-            movement.StartJump(player.transform.position, 1f);
+            movement.StartJump(player.transform.position, jumpTime);
         }
         else if (Shortcuts.IsoToReal(player.transform.position - transform.position).sqrMagnitude < 7 * 7)
         {
@@ -49,50 +44,7 @@ public class Rat : MonoBehaviour
             movement.SetTargetMoveDir(Vector2.zero);
         }
 
-        //GetComponentInChildren<Animator>().SetFloat("WalkDir", ((Vector2.SignedAngle(Vector2.right, movement.GetWalkDir())+360f)%360f)/360f); // Not PH
-        GetComponentInChildren<Animator>().SetFloat("WalkDir", ((Vector2.SignedAngle(Vector2.right, movement.GetMoveDir())+360f)%360f)/360f); //PH
+        //GetComponentInChildren<Animator>().SetFloat("WalkDir", ((Vector2.SignedAngle(Vector2.right, movement.GetWalkDir()) + 360f) % 360f) / 360f); // Not PH
+        GetComponentInChildren<Animator>().SetFloat("WalkDir", ((Vector2.SignedAngle(Vector2.right, movement.GetMoveDir()) + 360f) % 360f) / 360f); //PH
     }
 }
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//[RequireComponent(typeof(NPCMovement))]
-//[RequireComponent(typeof(Health))]
-//public class Rat : MonoBehaviour
-//{
-//    [SerializeField] private GameObject player;
-//    private NPCMovement movement;
-//    private Health health;
-
-//    private void Awake()
-//    {
-//        movement = GetComponent<NPCMovement>();
-//        health = GetComponent<Health>();
-
-//        //Subscribing to events - PH, should be abstracted to not write this every time for a new enemy
-//        health.OnKnockbackReceived += movement.StartKnockback;
-//    }
-
-//    //public bool isMoving = false; //TEST
-//    //private void Start()
-//    //{
-//    //    movement.SetIsMoving(isMoving);
-//    //}
-
-//    private void Update()
-//    {
-//        if (Shortcuts.IsoToReal(player.transform.position - transform.position).sqrMagnitude < 7 * 7)
-//        {
-//            movement.SetTarget(player.transform.position);
-//        }
-//        else
-//        {
-//            movement.SetTarget(transform.position);
-//        }
-
-//        GetComponentInChildren<Animator>().SetFloat("WalkDir", ((Vector2.SignedAngle(Vector2.right, movement.GetWalkDir()) + 360f) % 360f) / 360f);
-//    }
-//}
-
