d723e703afa66d0fa5b29d1c335077c09d855f10
Zhizhoid <misha2006.mk@gmail.com>
2024-01-10 11:12:03 +0200

    Change TestSavable's saved data to try saving position to a file; Add SerializableVector3;

--- Prototype/Assets/Experimental/TestSavable.cs
+++ Prototype/Assets/Experimental/TestSavable.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TestSavable : MonoBehaviour, ISavable
 {
+    [System.Serializable] private class State
+    {
+        public SerializableVector3 position;
+        public string someData;
+    }
+
     [SerializeField] private string someData;
 
     public object GetState()
     {
-        return someData;
+        State state = new();
+        state.position = new SerializableVector3(transform.position);
+        state.someData = someData;
+        return state;
     }
 
-    public void LoadState(object state)
+    public void LoadState(object stateObj)
     {
-        someData = (string)state;
+        State state = (State)stateObj;
+        someData = state.someData;
+        transform.position = state.position.GetVector3();
     }
 }
