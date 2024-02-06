efd8a48e095a9d81e0597d999439d46959be74a5
Zhizhoid <misha2006.mk@gmail.com>
2024-02-06 17:38:07 +0200

    Start working on interactable object pulsing near player

--- Prototype/Assets/Scripts/Environment/Interactable.cs
+++ Prototype/Assets/Scripts/Environment/Interactable.cs
@@ -6,11 +6,56 @@ public abstract class Interactable : MonoBehaviour
 {
     [SerializeField] private float interactionRadius = 0f;
 
+    [Header("Pulse")]
+    [SerializeField] private bool pulseWhenInteractable = true;
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField][Range(0, 1)] private float maxWhitening = 1f;
+    [SerializeField] private float pulsePeriod = 0.5f;
+
+    private bool isClosestInteractable = true;
+    private float pulseStartTime;
+    private Color startColor;
+
     // NOTE: If NPCs can also interact, PlayerInteractions should be replaced with InteractionAgent abstract class or interface
     public abstract void Interact(PlayerInteractions interactionAgent);
     public float GetInteractionRadius()
     {
         return interactionRadius;
+    }
+
+    public void SetIsClosestInteractable(bool newIsClosestInteractable)
+    {
+        if (!newIsClosestInteractable)
+        {
+            isClosestInteractable = false;
+            sprite.color = startColor;
+            return;
+        }
+
+        if (isClosestInteractable) return;
+
+        isClosestInteractable = true;
+        pulseStartTime = Time.time;
+    }
+
+    private void Start()
+    {
+        if (pulseWhenInteractable)
+        {
+            startColor = sprite.color;
+        }
+    }
+
+    private void Update()
+    {
+        if(isClosestInteractable && pulseWhenInteractable)
+        {
+            //float whitening = maxWhitening * Mathf.Sin(((Time.time - pulseStartTime) * 2 * Mathf.PI) / pulsePeriod);
+            float whitening = maxWhitening * (1 + Mathf.Sin(((Time.time - pulseStartTime) * 2 * Mathf.PI) / pulsePeriod - Mathf.PI / 2)) / 2;
+            Debug.Log(new Color(startColor.r + whitening, startColor.g + whitening, startColor.b + whitening, sprite.color.a));
+            //Debug.Log($"{name}: {startColor}    {sprite}");
+            sprite.color = new Color(startColor.r + whitening, startColor.g + whitening, startColor.b + whitening, sprite.color.a);
+        }
     }
 
     private void OnDestroy()
