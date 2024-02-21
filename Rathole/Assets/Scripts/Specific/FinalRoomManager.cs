421ea5cd256a08ae4c5b60b82283d35190054ebb
Zhizhoid <misha2006.mk@gmail.com>
2024-02-21 22:12:38 +0200

    Add music fade out when going through final door

--- Rathole/Assets/Scripts/Specific/FinalRoomManager.cs
+++ Rathole/Assets/Scripts/Specific/FinalRoomManager.cs
@@ -6,11 +6,25 @@ using UnityEngine.SceneManagement;
 public class FinalRoomManager : MonoBehaviour
 {
     [SerializeField] private EndingCutsceneManager.Ending ending = EndingCutsceneManager.Ending.Caught;
+    [SerializeField] private float endingCutsceneDelay = 2f;
+    [SerializeField] private GameObject door;
 
     private void Start()
     {
+        door.SetActive(false);
+        PauseManager.SetManualPauseAllowed(false);
+        GameManager.GetPlayer().GetComponent<PlayerHealth>().SetHealthUIVisible(false);
+        GameManager.GetPlayer().SetActive(false);
         EndingCutsceneManager.ending = ending;
         FinalScreenManager.foundWeapon = GameManager.GetPlayer().GetComponent<PlayerCombat>().HasWeapon();
-        GameManager.LoadEndingCutsceneScene();
+        
+        IEnumerator waitAndLoadEndingCutsceneScene()
+        {
+            yield return new WaitForSeconds(endingCutsceneDelay);
+            GameManager.LoadEndingCutsceneScene();
+        }
+        StartCoroutine(waitAndLoadEndingCutsceneScene());
+
+        SoundManager.FadeOutSoundtrack(endingCutsceneDelay);
     }
 }
